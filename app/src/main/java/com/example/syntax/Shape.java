4e0261b764da7a24a8d72afa5495337454a78f6d
yixingli <yixing6@ualberta.ca>
2026-01-29 20:09:08 -0700

    colour1

--- app/src/main/java/com/example/syntax/Shape.java
+++ app/src/main/java/com/example/syntax/Shape.java
@@ -3,7 +3,7 @@ package com.example.syntax;
 public abstract class Shape {
     private Integer x;
     private Integer y;
-    private String color = "blue";
+    private String color1 = "blue";
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
