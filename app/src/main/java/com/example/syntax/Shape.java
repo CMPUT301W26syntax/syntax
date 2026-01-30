188a02e6953afedf7f6e19ff53fb8ceaffdcb7e8
anupam5671 <anupam4@ualberta.ca>
2026-01-30 15:51:41 -0700

    Add color field to Shape

--- app/src/main/java/com/example/syntax/Shape.java
+++ app/src/main/java/com/example/syntax/Shape.java
@@ -3,7 +3,7 @@ package com.example.syntax;
 public abstract class Shape {
     private Integer x;
     private Integer y;
-    String color = "blue";
+    String color = "lilac";
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
