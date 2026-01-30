116bd3c66e032c8148cc118443ac22707ad7bdeb
anupam5671 <anupam4@ualberta.ca>
2026-01-30 16:15:32 -0700

    Set color on Anupam branch

--- app/src/main/java/com/example/syntax/Shape.java
+++ app/src/main/java/com/example/syntax/Shape.java
@@ -3,7 +3,7 @@ package com.example.syntax;
 public abstract class Shape {
     private Integer x;
     private Integer y;
-    String color = "lilac";
+    String color = "lavender";
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
