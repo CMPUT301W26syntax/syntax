e739523f1039338676e7b557132b26d0e30acbbc
VredstarX <130722153+VredstarX@users.noreply.github.com>
2026-01-30 00:25:31 -0700

    Red

--- app/src/main/java/com/example/syntax/Shape.java
+++ app/src/main/java/com/example/syntax/Shape.java
@@ -3,6 +3,7 @@ package com.example.syntax;
 public abstract class Shape {
     private Integer x;
     private Integer y;
+    String color = "Red";
 
     public Shape(Integer x, Integer y) {
         this.x = x;
