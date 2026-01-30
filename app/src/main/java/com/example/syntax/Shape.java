29c47de1c582d6a6f40f8893133b93ca32c71cf8
Liazard007 <146795195+Liazard007@users.noreply.github.com>
2026-01-30 15:28:01 -0700

    Update Shape.java
    Redoing it

--- app/src/main/java/com/example/syntax/Shape.java
+++ app/src/main/java/com/example/syntax/Shape.java
@@ -3,7 +3,6 @@ package com.example.syntax;
 public abstract class Shape {
     private Integer x;
     private Integer y;
-    String color = "Red";
     
     public Shape(Integer x, Integer y) {
         this.x = x;
